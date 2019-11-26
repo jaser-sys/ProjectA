3b72f476db2e653057335b229e7ad9e8ad233f92
Amein Altory <amein2ry@outlook.com>
2019-11-26 15:10:19 +0200

    changing CSV file

--- products.py
+++ products.py
@@ -3,7 +3,7 @@ csv.register_dialect('myDialect',
 delimiter = ',',
 skipinitialspace=True)
 
-with open('customers.csv', 'r') as csvFile:
+with open('products.csv', 'r') as csvFile:
     reader = csv.reader(csvFile, dialect='myDialect')
     for row in reader:
         print(row)
@@ -13,18 +13,18 @@ csvFile.close()
 
 
 rowNum=input("Enter ID: ")
-product=input("product")
+product=input("product: ")
 
 
 
 row = [rowNum, product]
 
-with open('customers.csv', 'r') as readFile:
+with open('products.csv', 'r') as readFile:
     reader = csv.reader(readFile)
     lines = list(reader)
     lines[2] = row
 
-with open('customers.csv', 'w') as writeFile:
+with open('products.csv', 'w') as writeFile:
     writer = csv.writer(writeFile)
     writer.writerows(lines)
 
